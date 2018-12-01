04abdc0f22e665966fcc8a5b9a15ab232b4878bb
pavbaranov <pavbaranov@gmail.com>
2018-12-01 21:07:50 +0100

    Add kf5 5.53rc

--- kf5rc/podbijanie_wersji_rc.sh
+++ kf5rc/podbijanie_wersji_rc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find -name PKGBUILD -exec sed -i 's/pkgver=5.48rc/pkgver=5.50rc/g' {} \;
-find -name PKGBUILD -exec sed -i 's/v5.48.0-rc1/v5.50.0-rc1/g' {} \;
+find -name PKGBUILD -exec sed -i 's/pkgver=5.52rc/pkgver=5.53rc/g' {} \;
+find -name PKGBUILD -exec sed -i 's/v5.52.0-rc1/v5.53.0-rc1/g' {} \;
