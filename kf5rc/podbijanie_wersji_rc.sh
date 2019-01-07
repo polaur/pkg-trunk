1682f638a9d7f3c5f18e430bd456bb22ea677857
pavbaranov <pavbaranov@gmail.com>
2019-01-07 11:26:20 +0100

    Upgrade KF5rc to 5.54rc1

--- kf5rc/podbijanie_wersji_rc.sh
+++ kf5rc/podbijanie_wersji_rc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find -name PKGBUILD -exec sed -i 's/pkgver=5.52rc/pkgver=5.53rc/g' {} \;
-find -name PKGBUILD -exec sed -i 's/v5.52.0-rc1/v5.53.0-rc1/g' {} \;
+find -name PKGBUILD -exec sed -i 's/pkgver=5.53rc/pkgver=5.54rc/g' {} \;
+find -name PKGBUILD -exec sed -i 's/v5.53.0-rc1/v5.54.0-rc1/g' {} \;
